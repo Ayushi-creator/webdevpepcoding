90c49d9fd639379a2a34dbf9f533ec42b125e770
Ayushi-creator <sharmaayushi62998@gmail.com>
2022-03-25 11:00:20 +0530

    init

---
+++ 1_Js introduction/1_variabel.js
@@ -0,0 +1,103 @@
+//Varibel declaration
+//------------------------------------------
+//var,let,const
+//--------------------------------------
+//var a;
+//console.log(a)// opt ->undefined
+//-----------------------------------
+//var a=2;
+//console.log(a)//opt->2
+//--------------------------------------
+// var a='i am a string'//2
+//console.log(a)
+//opt->2
+//opt->i am a string
+//yaha var a ko baar baar redeclare kar pa rahe par asa javaa mei nahi hota hai
+//-----------------------------------------------------------------------------------
+//var a=2;
+//yaha par bhi a ki value 2
+//yaha par bhi a ki value 2
+//bahut sara code likha hai yaha
+
+// var a='i am a string'
+//console.log(a)//i am a string
+//now I get a value i am a string but i want 2
+
+//ab a ki value dono nahi milegi  i am string vali milegi last wali milegi so this is not crct bcz we want to use both var a
+//us var keyword par no. bhi dal rahe or string bhi
+//------------------------------------------------------------------------
+//VAR KEYWORD SHORTCOMING
+    //JS IS A DYNAMICALLY TYPED LANGUAGE DATATYPE IS NOT SPECEFIED HERE
+
+//    var a;
+  //  console.log(a)
+
+ //   var a=3;
+ //   console.log(a)
+
+ // var  a=" i am a string" ;
+ //   console.log(a)
+
+ //   var a=true;
+ //   console.log(a)
+
+  //  var a=null;
+  //  console.log(a)
+ // ------------------------------------------------------------------------------------
+
+ //Datatype in javascript-number,string,boolean,undefined ,null
+
+//var b=2.3//as a no. no floating,doubel here
+//var c='d';//string no character
+//var d='i am a string';//string
+//-----------------------------------------------------------------------------
+
+//var has some problem
+
+    //1st-problem with var is redeclaration
+    //=========================================
+    //var b='hello'
+    //var b='hi'
+    //console.log(b)//hi
+
+    //overcoming redeclaration
+  //  ================================
+    //use let keyword
+    //==================================
+
+    //let b='hello'
+    //console.log(b)
+    //let b='hi'//redeclaration
+    //console.log(b)//SyntaxError: Identifier 'b' has already been declared
+//multipel redeclaration not allowed
+
+//let b='hello'
+ //b='hi'
+//console.log(b)//hi
+
+
+//let b='hello'
+//console.log(b)//hello
+// b='hi'//reassigning is allowed
+//console.log(b)//hi
+
+// var a;
+// console.log(a);//undefined
+// let b;
+// console.log(b);//undefined
+//const c;
+//console.log(c);// Missing initializer
+
+
+
+
+
+
+
+    
+       
+
+       
+
+
+
---
+++ 1_Js introduction/2_functions.js
@@ -0,0 +1,36 @@
+///NORMAL FUNCTION
+//function sayHi(){
+  //  console.log('function said hi')
+//}//function declaration
+//sayHi()// function invokation
+
+//parameters
+//function add(a,b){// a and b are parameters
+   // console.log("the addition is",a+b)
+//}
+//add(2,3)//2 and 3 are arguments
+//multiply
+//function multiply(a,b){
+ //   return a*b
+//}
+//console.log(multiply(2,3))
+////////////////////////////////////////or//////////////////////////
+//function multiply(a,b){
+  //  return a*b
+//}
+//let product=multiply(2,3)
+//console.log(product)
+//function as first class citizens
+// no difference in function and variabel
+//let sayHel=function(){//anonymus function
+  //  console.log('hi')
+//}//function expression
+//sayHel()
+
+//in js u can assign a function to a variabel and can call it
+
+//IIFE(Imediately invoked function expression)
+//let add=(function(a,b){
+   // return a+b;
+//})(10,20)
+//console.log(add)
