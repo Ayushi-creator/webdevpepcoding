fbbdb03c97bf230fc642b659d86f3b875df3f156
Ayushi-creator <sharmaayushi62998@gmail.com>
2022-03-26 07:24:20 +0530

    init

---
+++ 1_Js introduction/3_array.js
@@ -0,0 +1,69 @@
+//Array provides you an odered collection of elements
+
+// let arr=[]//array declaration
+// let cars=['ferrari','bmw','tesla','jaguar']//array initialized
+// const car=['ferrari',2,true,null]
+// console.log(car)
+// console.log(cars)
+// console.log('this is a',car)
+// //in js u can store different values of different datatype
+
+//let cars2=['ferrari','jaguar','bmw','tesla']
+//accesing the elements of an array
+// console.log(cars2[0])
+// console.log(cars2[1])
+// console.log(cars2[2])
+// console.log(cars2[3])
+
+//replace the element of an array
+// cars2[2]='betely'//2nd index will be replaced with this value
+// console.log(cars2)
+
+//add new element to an array
+// cars2[4]='mercedes'
+// console.log(cars2)
+
+// cars2[6]='nano'
+// console.log(cars2)//[ 'ferrari', 'jaguar', 'bmw', 'tesla', <2 empty items>, 'nano' ]
+
+//array method
+
+//pop method-this method removes the last element from the array
+// let cars2=['ferrari','jaguar','bmw','tesla']
+// console.log(cars2.pop())//tesla
+// console.log(cars2)//[ 'ferrari', 'jaguar', 'bmw' ]
+
+//push method-this add element at the end of an array
+// let cars2=['ferrari','jaguar','bmw','tesla']
+// cars2.push('maruti')
+// console.log(cars2)//[ 'ferrari', 'jaguar', 'bmw', 'tesla', 'maruti' ]
+
+//shift-removes the starting element from the array
+// let cars2=['ferrari','jaguar','bmw','tesla']
+// console.log(cars2.shift())//ferrrari
+// console.log(cars2)//[ 'jaguar', 'bmw', 'tesla' ]
+
+//unshift- add the element in the starting of the array
+// let cars2=['ferrari','jaguar','bmw','tesla']
+// cars2.unshift('tractor')
+// console.log(cars2)//[ 'tractor', 'ferrari', 'jaguar', 'bmw', 'tesla' ]
+
+//length of an array
+// let cars2=['ferrari','jaguar','bmw','tesla']
+// console.log(cars2.length)//4
+
+//multidimensional array
+// let matrix=[
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+// ]
+// console.log(matrix[0][0])//1
+// console.log(matrix[0])//[ 1, 2, 3 ]
+
+
+
+
+
+
+
