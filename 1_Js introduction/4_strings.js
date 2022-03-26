0dc9f8f7fa1c0d59f19ffb37a406cef36814fcfc
Ayushi-creator <sharmaayushi62998@gmail.com>
2022-03-26 10:38:11 +0530

    string

---
+++ 1_Js introduction/4_strings.js
@@ -0,0 +1,62 @@
+//string is a sequence of characters
+// let str='pepcoders'
+// console.log(str)//pepcoders
+
+//methods
+//length
+// let str='pepcoders'
+// console.log(str.length)//9
+
+//extracting the part of string
+
+let str='pepcoders'
+//we want only code
+
+//1.slice
+//(start,end+1)
+//let slicedstr=str.slice(3,7)
+//console.log(slicedstr)//code
+
+//coder from the string
+// let slicedstr=str.slice(3,8)
+// console.log(slicedstr)
+
+//2.substr
+//extract code
+//substr(start,length)
+// let substring=str.substr(3,4)
+// console.log(substring);//code
+
+// let substring=str.substr(3)
+// console.log(substring)//coders   
+
+//replacing string content
+//replace method
+// let sayHello='hello ayushi'
+// console.log(sayHello)//hello ayushi
+// let sayBye=sayHello.replace('hello','bye')
+// console.log(sayBye)//bye ayushi
+
+//ToUpperCase and ToLowerCase
+// let text='hello world'
+// let text2=text.toUpperCase()
+// console.log(text2)//HELLO WORLD
+
+// let text1='Hello World'
+// let text2=text1.toLowerCase()
+// console.log(text2)//hello world
+
+//concatenation(concat method)
+// let firstString='hello'
+// let secondString='Ayushi'
+// let concatenation=firstString.concat(' ',secondString)
+// console.log(concatenation)//hello Ayushi
+
+//Trim method is used to remove all the white spaces
+// let text='         hello world      '
+// console.log(text)//        hello world    
+// console.log(text.trim())//hello world
+
+
+
+
